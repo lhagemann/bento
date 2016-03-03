e48fd042983deef0309976786e72bc75892280e5
Lisa Hagemann <lhagemann@dyn.com>
2016-03-03 09:27:16 -0500

    use ssh user for /tmp/* ownership; upload directory contents

--- scripts/chef/bootstrap.sh
+++ scripts/chef/bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Initial Downloads and config
 
-sudo mkdir -p /tmp/chef && sudo chown -R ubuntu:ubuntu /tmp/chef
-sudo mkdir -p /tmp/config && sudo chown -R ubuntu:ubuntu /tmp/config
+sudo mkdir -p /tmp/chef && sudo chown -R vagrant:vagrant /tmp/chef
+sudo mkdir -p /tmp/config && sudo chown -R vagrant:vagrant /tmp/config
 sudo mkdir -p /etc/chef && sudo chown -R root:root /etc/chef
-sudo mkdir -p /tmp/packer-chef-client && sudo chown -R ubuntu:ubuntu /tmp/packer-chef-client
+sudo mkdir -p /tmp/packer-chef-client && sudo chown -R vagrant:vagrant /tmp/packer-chef-client
 sudo mkdir -p /etc/chef/ohai/hints
