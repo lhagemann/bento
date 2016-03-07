63ba35b782ee615bbfdb12e0e23e86883c5f305b
Lisa Hagemann <lhagemann@dyn.com>
2016-03-07 16:56:41 -0500

    add in chef-client provisioner; point local chef repo at a simple repo

--- scripts/chef/cleanup.sh
+++ scripts/chef/cleanup.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash -x
 # Remove items used for building, since they aren't needed anymore
 
 sudo apt-get -y autoremove
 sudo apt-get clean
 
 #Clean up tmp
-#sudo rm -rf /tmp/*
+sudo rm -rf /tmp/*
 
 # reassign /etc/chef
 sudo chown -R root:root /etc/chef
